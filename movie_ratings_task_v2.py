1cd6d7ae376dc88d87aa2283eefd91e3e49fe71e
BenasJasilionis <benas.jasilionis@gmail.com>
2023-02-01 16:42:27 +0000

    completed movie rating v2 task

--- movie_ratings_task_v2.py
+++ movie_ratings_task_v2.py
@@ -1,13 +1,13 @@
 def movie_rating(age: int):
     if age < 1 or age > 117:
         print("Please enter an age between 1 and 117")
-    elif age >= 1 and age < 8:
+    elif 1 <= age < 8:
         print(f"You are {age} and are able to watch U rated films")
-    elif age >= 8 and age < 12:
+    elif 8 <= age < 12:
         print(f"Your are {age} and are able to watch U and PG rated films")
-    elif age >= 12 and age < 15:
+    elif 12 <= age < 15:
         print(f"Your are {age} and are able to watch U, PG and 12A rated films")
-    elif age >= 15 and age < 18:
+    elif 15 <= age < 18:
         print(f"Your are {age} and are able to watch U, PG, 12A and 15 rated films")
     elif age >= 18:
         print(f"Your are {age} and are able to watch any film you like!")
