49faa508721c85611a991506caab384db9d241d2
BenasJasilionis <benas.jasilionis@gmail.com>
2023-02-01 16:35:29 +0000

    completed movie rating v2 task

--- movie_ratings_task_v2.py
+++ movie_ratings_task_v2.py
@@ -1,20 +1,19 @@
-age = int(input("What is your age?"))
-
-
-def movie_rating_invalid(age):
-    if age.isdigit() and int(age) < 1 and int(age) > 117 :
+def movie_rating(age: int):
+    if age < 1 or age > 117:
         print("Please enter an age between 1 and 117")
-
-
-    elif age.isdigit() and int(age) >= 1 and int(age) < 8:
+    elif age >= 1 and age < 8:
         print(f"You are {age} and are able to watch U rated films")
-    elif age.isdigit() and int(age) >= 8 and int(age) < 12:
+    elif age >= 8 and age < 12:
         print(f"Your are {age} and are able to watch U and PG rated films")
-    elif age.isdigit() and int(age) >= 12 and int(age) < 15:
+    elif age >= 12 and age < 15:
         print(f"Your are {age} and are able to watch U, PG and 12A rated films")
-    elif age.isdigit() and int(age) >= 15 and int(age) < 18:
+    elif age >= 15 and age < 18:
         print(f"Your are {age} and are able to watch U, PG, 12A and 15 rated films")
-    elif age.isdigit() and int(age) >= 18:
+    elif age >= 18:
         print(f"Your are {age} and are able to watch any film you like!")
     else:
         print("Please enter a valid age using numbers between 1 and 117")
+
+
+user_age = int(input("How old are you?"))
+movie_rating(user_age)
